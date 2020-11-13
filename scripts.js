d6cd01ad5c08ef1f41d61a00aa4cdfaa0c442703
Remi LAK <remi.lak@new-immo-group.com>
2020-11-13 11:28:28 +0100

    accordion

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$(".faq-question.q-1").click(() => {
+    $(".faq-answer.a-1").toggleClass("show");
+    $(".faq-question.q-1").toggleClass("bold");
+});
+
+$(".faq-question.q-2").click(() => {
+    $(".faq-answer.a-2").toggleClass("show");
+    $(".faq-question.q-2").toggleClass("bold");
+});
+
+$(".faq-question.q-3").click(() => {
+    $(".faq-answer.a-3").toggleClass("show");
+    $(".faq-question.q-3").toggleClass("bold");
+});
+
+$(".faq-question.q-4").click(() => {
+    $(".faq-answer.a-4").toggleClass("show");
+    $(".faq-question.q-4").toggleClass("bold");
+});
+
+$(".faq-question.q-5").click(() => {
+    $(".faq-answer.a-5").toggleClass("show");
+    $(".faq-question.q-5").toggleClass("bold");
+});
